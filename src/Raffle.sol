de19b5f69109dc0b69d0ec25b47ac5761470cab5
root <root@DESKTOP-0CK23OM.>
2025-02-24 14:37:35 +0100

    simple lottery contract v1

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -7,38 +7,112 @@ pragma solidity ^0.8.19;
 
 contract SimpleLottery {
 
+
+    error Raffle__MoreFundsNeededToEnterRaffle();
+    error Raffle__NoPlayersEnteredRaffle();
+    error Raffle__PrizeTransferToWinnerFailed();
+    error Raffle__OnlyManagerCanStartRaffle();
+    error Raffle__WithdrawalFailed();
+
+
     address public manager;
-    address[] public players;
+    address[] private s_players;
     address[] public winners;
+    uint256 public lotteryId;
+
+    event PlayerEntered(address indexed player, uint256 amount);
+    event WinnerPicked(address indexed winner, uint256 amount, uint256 lotteryId);
+
+
 
     constructor () {
         manager = msg.sender;
+        lotteryId = 1;
     }
 
 
     function enterLottery() public payable {
 
-        require(msg.value >= 0.01 ether, "Minimum 0.01 ETH required to enter");
-        players.push(msg.sender);
+        if (msg.value < 0.01 ether) {
+            revert Raffle__MoreFundsNeededToEnterRaffle();
+        }
+        
+        s_players.push(msg.sender);
+
+        emit PlayerEntered(msg.sender, msg.value);
 
     }
 
 
     receive() external payable {
+        if(msg.value < 0.01 ether) {
+            revert Raffle__MoreFundsNeededToEnterRaffle();
+        }
+
         enterLottery();
     }
 
 
-    function pickWinner () public {
-        require(msg.sender == manager, "Only manager can pick a winner");
-        require(players.length > 0, "No players in the lottery");
+    function pickWinner () public onlyManager {
+        if (s_players.length == 0) {
+            revert Raffle__NoPlayersEnteredRaffle();
+        }
 
-        uint256 winnerIndex = uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, players.length))) % players.length;
-        address winner = players[winnerIndex];
+        uint256 winnerIndex = random() % s_players.length;
+        address winner = s_players[winnerIndex];
         winners.push(winner);
 
-        payable(winner).transfer(address(this).balance);
-        players = new address[](0);
+        uint256 prizeAmount = address(this).balance;
+
+        s_players = new address[](0);
+        lotteryId++;
+        
+        (bool success, ) = payable(winner).call{value: prizeAmount}("");
+        if (!success) {
+            revert Raffle__PrizeTransferToWinnerFailed();
+        }
+
+        emit WinnerPicked(winner, prizeAmount, lotteryId);
+    }
+
+
+    function withdrawFunds() public onlyManager {
+        uint256 balance = address(this).balance;
+
+        if(balance == 0) {
+            revert Raffle__WithdrawalFailed();
+        }
+
+        (bool success, ) = payable(manager).call{value: balance}("");
+
+        if(!success) {
+            revert Raffle__WithdrawalFailed();
+        }
+    }
+
+
+
+    function getBalance() public view returns(uint256) {
+        return address(this).balance;
+    }
+
+
+    function getPlayers() public view returns(address[] memory) {
+        return s_players;
+    }
+
+
+    function random() private view returns(uint256) {
+        return uint256(keccak256(abi.encodePacked(block.timestamp, block.prevrandao, s_players.length)));
+    }
+
+
+
+    modifier onlyManager {
+        if (msg.sender != manager) {
+            revert Raffle__OnlyManagerCanStartRaffle();
+        }
+        _;
     }
 
 }
